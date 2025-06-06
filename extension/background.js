99ff21e07ec6f9f4211dd13109b4478a71a5329b
Simar Malhotra <malhotrasimar009@gmail.com>
2025-06-06 10:55:49 -0400

    added support to be able to invoke hotkeys for psu library catalog search

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,7 @@
 const commandToUrl = {
   "search-wikipedia": "https://en.wikipedia.org/wiki/Special:Search?search=",
-  "search-jisho": "https://jisho.org/search/"
+  "search-jisho": "https://jisho.org/search/",
+  "search-psu-lib":"https://libraries.psu.edu/search/all/"
 };
 
 chrome.commands.onCommand.addListener((command) => {
@@ -13,7 +14,7 @@ chrome.commands.onCommand.addListener((command) => {
         func: () => window.getSelection().toString()
       },
       (results) => {
-        const selectedText = results[0]?.result.trim();
+        const selectedText = results[0]?.result.trim().toLowerCase();
         if (selectedText) {
           const url = commandToUrl[command] + encodeURIComponent(selectedText);
           chrome.tabs.create({ url });
