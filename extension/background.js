bb071acb47a16f8cabf8c973b5556532d68d6822
Simar Malhotra <malhotrasimar009@gmail.com>
2025-06-06 10:43:58 -0400

    improve readability by removing hardcoded strings

--- extension/background.js
+++ extension/background.js
@@ -1,33 +1,24 @@
+const commandToUrl = {
+  "search-wikipedia": "https://en.wikipedia.org/wiki/Special:Search?search=",
+  "search-jisho": "https://jisho.org/search/"
+};
+
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "search-wikipedia") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        func: () => window.getSelection().toString()
-      }, (results) => {
-        const selectedText = results[0].result.trim();
-        if (selectedText) {
-          const url = `https://en.wikipedia.org/wiki/Special:Search?search=${encodeURIComponent(selectedText)}`;
-          chrome.tabs.create({ url });
-        }
-      });
-    });
-  
-  } 
+  if (!(command in commandToUrl)) return;
 
-  if (command === "search-jisho") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript(
+      {
         target: { tabId: tabs[0].id },
         func: () => window.getSelection().toString()
-      }, (results) => {
-        const selectedText = results[0].result.trim();
+      },
+      (results) => {
+        const selectedText = results[0]?.result.trim();
         if (selectedText) {
-          const url = `https://jisho.org/search/${encodeURIComponent(selectedText)}`;
+          const url = commandToUrl[command] + encodeURIComponent(selectedText);
           chrome.tabs.create({ url });
         }
-      });
-    });
-  }
+      }
+    );
+  });
 });
-
